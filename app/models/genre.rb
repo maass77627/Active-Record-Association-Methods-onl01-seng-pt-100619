e07a7964dafed5ebb1c0393bdc557b770db60343
kelly r maass <kmaass77627@gmail.com>
2020-01-28 02:27:01 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -13,6 +13,6 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artist.collect
+    self.artists.collect do |t|
   end
 end
